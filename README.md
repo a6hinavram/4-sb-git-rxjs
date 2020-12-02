ec8177c99e806b54ca8ba21ac36115033f72dff2
a6hinavram <a6hinavram@gmail.com>
2020-12-03 00:55:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 4-sb-git-rxjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/4-sb-git-rxjs)
---
+++ index.ts
@@ -0,0 +1,9 @@
+import { Observable } from 'rxjs';
+
+var observer = new Observable(
+   function subscribe(subscriber) {
+      subscriber.next("My First Observable")
+   }
+);
+
+observer.subscribe(x => console.log(x));
